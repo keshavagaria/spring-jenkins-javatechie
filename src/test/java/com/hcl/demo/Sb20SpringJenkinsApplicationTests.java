8ff6fd26e1117e7167762ad0630ac90d4d66abcd
Vishal Singh <vishal267s@gmail.com>
2025-09-01 18:28:30 +0530

    build pass

--- src/test/java/com/hcl/demo/Sb20SpringJenkinsApplicationTests.java
+++ src/test/java/com/hcl/demo/Sb20SpringJenkinsApplicationTests.java
@@ -13,7 +13,7 @@ class Sb20SpringJenkinsApplicationTests {
 	static Logger logger = LoggerFactory.getLogger(Sb20SpringJenkinsApplication.class);
 	
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 		logger.info("Test Case Executing...");
 		logger.info("Test Case Executing again and again...");
 		
